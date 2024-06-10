5ab0d64570bdc31341fa77ee31aea62c451046af
Benjamin Bachs <benjamin.bachs@gmail.com>
2024-06-10 00:31:54 -0300

    punto1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"Ingrese un numero para ser invertido: ");
+string entrada = Console.ReadLine();
+int convertido;
+
+if(int.TryParse(entrada, out convertido) && convertido>0){
+    int aux = 0;
+    while (convertido != 0)
+    {
+        aux=aux*10+convertido%10;
+        convertido/=10;
+    }
+    Console.WriteLine($"Numero invertido: {aux}");
+}
